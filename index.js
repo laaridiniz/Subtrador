358c669779636f6998ba2eb669180fb7904ea202
Larissa Diniz <diniz.laari@gmail.com>
2022-09-02 22:14:41 -0300

    testeModulo

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Subtrador {
+    subtrair(numero1, numero2) {
+        return numero1 - numero2;
+    }
+}
+exports.default = Subtrador;
